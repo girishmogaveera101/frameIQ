a50188e2a9a5fff5166fbe375d2498f75a8aa0a6
Girish Mogaveera <girishmogaveera101@gmail.com>
2025-05-27 01:04:32 -0500

    seo setmaps and robots.txt 1

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://frameiq.vercel.app', // your actual deployed site
+  generateRobotsTxt: true,
+};
+
+module.exports = config;
