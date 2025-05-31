c66689c5fbf2c90d4574f4374843b85e03268892
Girish Mogaveera <girishmogaveera101@gmail.com>
2025-05-31 23:09:13 +0530

    feat: leaderboard frntend1

--- app/components/leaderboard.tsx
+++ app/components/leaderboard.tsx
@@ -1,10 +1,43 @@
 "use client"
 
-import React from 'react'
+import React,{useEffect} from 'react'
 
 function leaderboard() {
+
+  useEffect(()=>{
+    findLeaderboardScore();
+  },[])
+
+  const findLeaderboardScore = async () => {
+    const response = await fetch('');
+    const resData = await response.json();
+    console.log(resData)
+  }
+
   return (
-    <div>leaderboard</div>
+    <div>
+      <p className="">Leaderboard</p>
+      <div className="">
+        <p className="">sl.no</p>
+        <p className="">username</p>
+        <p className="">score</p>
+      </div>
+      <div className="">
+        <p className="">sl.no</p>
+        <p className="">username</p>
+        <p className="">score</p>
+      </div>
+      <div className="">
+        <p className="">sl.no</p>
+        <p className="">username</p>
+        <p className="">score</p>
+      </div>
+      <div className="">
+        <p className="">sl.no</p>
+        <p className="">username</p>
+        <p className="">score</p>
+      </div>
+    </div>
   )
 }
 
