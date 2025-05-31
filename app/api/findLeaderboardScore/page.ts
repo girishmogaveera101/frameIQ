b9535df00bcb30698926a46993e4ba0bfb378040
Girish Mogaveera <girishmogaveera101@gmail.com>
2025-05-31 23:34:36 +0530

    feat: leaderboard backnd 1

---
+++ app/api/findLeaderboardScore/page.ts
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import clientPromise from "@/lib/mongodb";
+
+export async function GET() {
+  try {
+    const client = await clientPromise;
+    const db = client.db("frameFlix");
+    const data = await db.collection("users").find({}).sort({_id:-1}).limit(20).toArray();
+    // console.log("Fetched movies :", data);
+    return NextResponse.json(data);
+  }
+  catch (error) {
+    console.error("Could not fetch the movies", error);
+    return NextResponse.json(
+      { error: "could not fetch the movies" },
+      { status: 500 }
+    );
+  }
+}
