b2d505a1084c52d0cf8395f1068a325008880751
Girish Mogaveera <girishmogaveera101@gmail.com>
2025-06-25 19:49:03 +0530

    added profile images

---
+++ app/components/scoreBoard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function scoreBoard() {
+  return (
+    <div className=' w-[50%] md:ml-[25%] h-100 bg-violet-950 rounded-2xl shadow-2xl'>
+        <p className="text-center md:text-3xl">ScoreBoard</p>
+    </div>
+  )
+}
+
+export default scoreBoard
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import LatestMovies from './components/latestMovies'
 import Navbar from './components/navbar'
 import Heading from './components/heading'
 import HomeButtons from './components/homeButtons'
+import ScoreBoard from './components/scoreBoard'
 
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Heading />
       <LatestMovies />
       <HomeButtons />
+      <ScoreBoard/>
     </div>
   );
 }
